944f816a81c2def2a24d3e4f7b1c82cfbdb8f47d
Talles <tallesdecosta@gmail.com>
2024-11-16 08:05:25 -0300

    Revert "fix: consertei bugs nos workflows de produto"
    This reverts commit 4941c2f9ebe78b83199bbdbaf3a029a92521abd2.

--- src/html/js/visu-produto.js
+++ src/html/js/visu-produto.js
@@ -1,17 +1,11 @@
 
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
-nome = document.getElementById('nome');
-desc = document.getElementById('desc');
-disp = document.getElementById('disp');
-vol = document.getElementById('vol');
-veg = document.getElementById('veg');
-poso = document.getElementById('poso');
-alt = document.getElementById('alt');
-cat = document.getElementById('cat');
-contraindic = document.getElementById('contraindic');
-comp = document.getElementById('comp');
-
+nome = document.getElementById('nome')
+desc = document.getElementById('desc')
+disp = document.getElementById('disp')
+vol = document.getElementById('vol')
+veg = document.getElementById('veg')
 async function product(params) {
     
     resp = await fetch(`../php/produto.php?`+ new URLSearchParams({id:productId}), {
@@ -26,59 +20,5 @@ async function product(params) {
     disp.value = resp[0].disponibilidade
     vol.value = resp[0].volume
     veg.value = resp[0].eh_vegano
-    poso.value = resp[0].posologia
-    alt.value = resp[0].alt_text
-    cat.value = resp[0].categoria
-    contraindic.value = resp[0].contraindicacao
-    comp.value = resp[0].composicao
 }
 
-
-async function deleteProduct() {
-    
-    body = {
-        id: productId
-    }
-
-    response = await fetch('/php/produtos/delete.php', {
-
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify(body)
-
-    });
-
-    console.log(await response.json())
-
-}
-
-async function altProduct() {
-
-    body = {
-
-        nome: document.getElementById('nome').value,
-        desc: document.getElementById('desc').value,
-        disp: document.getElementById('disp').value,
-        vol: document.getElementById('vol').value,
-        veg: document.getElementById('veg').value,
-        poso: document.getElementById('poso').value,
-        alt: document.getElementById('alt').value,
-        cat: document.getElementById('cat').value,
-        contraindic: document.getElementById('contraindic').value,
-        comp: document.getElementById('comp').value,
-        id: productId
-
-    }
-
-    response = await fetch('/php/produtos/alterar.php', {
-
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify(body)
-
-    });
-
-    console.log(await response.json())
-
-    
-}
--- src/php/produtos/produtos.php
+++ src/php/produtos/produtos.php
@@ -1,77 +1,27 @@
 <?php
-include '../auth/criar-bd.php';
-session_start();
+include '.../auth/criar-bd';
 
     switch ($_SERVER['REQUEST_URI']) {
 
-        case '/php/produtos/produtos.php':
+        case '/php/produtos/produtos.php/consulta-individual':
             
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
                 
-                $dados = json_decode(file_get_contents('php://input'), true);
-    
+                $termo = $_GET['termo'];
                 $conn = conectarBanco();
-                
+
                 if (create_tables($conn) == true) 
                 {
 
-                    $conn -> begin_transaction();
-                    $arquivo = $dados['arquivo'];
-                    $alt_text = $dados['alt'];
-                    $pessoa_id = $_SESSION['id'];
-                    $nome = $dados['nome'];
-                    $composicao = $dados['comp'];
-                    $contraindicacao = $dados['contraindic'];
-                    $posologia = $dados['poso'];
-                    $descricao = $dados['desc'];
-                    $disponibilidade = $dados['disp'];
-                    $eh_vegano = $dados['veg']; 
-                    $volume = $dados['vol'];
-                    $categoria = $dados['cat'];
-
-                    
-                        
-                    $sql1 = "INSERT INTO imagem(arquivo, alt_text) VALUES (?, ?)";
-                    $st1 = $conn->prepare($sql1);
-                    $st1->bind_param('ss', $arquivo, $alt_text);
-
-
-                    if (!$st1->execute()) {
-                        throw new Exception("Error in imagem insert: " . $st1->error);
+        
                       
-                    }
+                } else 
+                {
 
-                    $image_id = $conn->insert_id;
+                    echo json_encode(['error' => 'erro interno do servidor']);
 
-                    $sql2 = "INSERT INTO produto(pessoa_id, imagem_id, nome, composicao, contraindicacao, posologia, descricao, disponibilidade, eh_vegano, volume, categoria, clickrate)
-                                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)";
-
-                    $st2 = $conn->prepare($sql2);
-                    $st2->bind_param('iissssssssi', $pessoa_id, $image_id, $nome, $composicao, $contraindicacao, $posologia, $descricao, $disponibilidade, $eh_vegano, $volume, $categoria);
-
-
-                    if (!$st2->execute()) {
-                        throw new Exception("Error in produto insert: " . $st2->error);
-                        
-                    }
-
-                        
-                    $conn->commit();
-                    echo json_encode("Records inserted successfully.");
-                    
-
-                    $st1->close();
-                    $st2->close();
-                    $conn->close();
-                            
-                            
-                        } else 
-                        {
-
-                            echo json_encode(['error' => 'erro interno do servidor']);
-
-                        }
+                }
 
 
 
